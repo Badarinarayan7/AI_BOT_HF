a7449c78a1d9d164ea1746d79bac8ea0d16ba16f
Badarinarayan7 <gaddanakerebadari7@gmail.com>
2026-04-15 14:44:53 +0530

    Update App.py

--- App.py
+++ App.py
@@ -7,3 +7,13 @@ summarizer = load_summarizer()
 st.title("AI Text Summarizer")
 st.write("Enter a long text below, and get a concise summary")
 long_text = st.text_area("Enter text to Summarize",height = 200)
+max_length = st.slider("Max summary Length",min_value = 50,max_value = 300,value = 130)
+min_length = st.slider("Min summary Length",min_value = 20,max_value = 100,value = 30)
+if st.button("Summarize"):
+  if long_text.strip():
+    with st.spinner("Generating Summary....."):
+      summary = summarizer(long_text,max_length=max_length,min_length=min_length,do_sample=False)
+      st.subheader("Summary:")
+      st.success(summary[0] ['summary_text'])
+  else:
+    st.warning("Please enter some text to summarize.")
