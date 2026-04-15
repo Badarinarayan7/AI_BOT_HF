012ecef44cb664b45ed0097e283708b21c3491d9
Badarinarayan7 <gaddanakerebadari7@gmail.com>
2026-04-15 14:28:21 +0530

    Create App.py

---
+++ App.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from transformers import pipeline
+st.cache_resource
+def load_summarizer():
+  return pipeline("summarization",model="sshleifer/distilbart-cnn-12-6")
+summarizer = load_summarizer()
+st.title("AI Text Summarizer")
+st.write("Enter a long text below, and get a concise summary")
+long_text = st.text_area("Enter text to Summarize",height = 200)
